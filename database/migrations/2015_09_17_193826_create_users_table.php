141b71853ebc3b9b8887927d80eec258eb349dc5
antonio becerra <antonio_sga@yahoo.es>
2015-09-27 21:37:18 +0100

    Migraciones base de datos

---
+++ database/migrations/2015_09_17_193826_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('fullname');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->string("foto",100)->default('default.png');
+            $table->bigInteger('rol_id')->unsigned();
+            $table->foreign('rol_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->boolean('activo')->default(true);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+
+}
