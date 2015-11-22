ac9543c78474c4de93f39c2de89f734d264767a5
fmentado <franciscomentadomanzanares@yahoo.es>
2015-11-22 20:54:12 +0000

    Modificación del js de las fechas de cursillos para asignarlas

--- public/js/comun/date.js
+++ public/js/comun/date.js
@@ -6,24 +6,22 @@ $(document).ready(function () {
     var now = new Date(fechaActualTemporal.getFullYear(),
         fechaActualTemporal.getMonth(),
         fechaActualTemporal.getDate(),
-        0, 0, 0, 0);
+        12, 0, 0, 0);
 
     var fechaInicio = $('#datepicker1').datepicker({
         onRender: function (date) {
             return date.valueOf() < now.valueOf() ? 'disabled' : '';
         }
     }).on('changeDate', function (evt) {
-        if (evt.date.valueOf() > fechaFinal.date.valueOf()) {
-            var newDate = new Date(evt.date)
-            newDate.setDate(newDate.getDate() + 1);
-            fechaFinal.setValue(newDate);
-        }
+        var newDate = new Date(evt.date)
+        newDate.setDate(newDate.getDate());
+        fechaFinal.setValue(newDate);
         fechaInicio.hide();
-        }).data('datepicker');
+    }).data('datepicker');
 
     var fechaFinal = $('#datepicker2').datepicker({
         onRender: function (date) {
-            return date.valueOf() <= (fechaInicio.date.valueOf() || now.valueOf()()) ? 'disabled' : '';
+            return date.valueOf() <= fechaInicio.date.valueOf() ? 'disabled' : '';
         }
     }).on('changeDate', function (evt) {
         fechaFinal.hide();
