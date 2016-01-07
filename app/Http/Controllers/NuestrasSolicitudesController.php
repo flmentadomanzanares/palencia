62ea90eaca40f7364e1cf1b6b1c56f43c124f29f
antonio becerra <antonio_sga@yahoo.es>
2016-01-07 17:55:52 +0000

    Merge con la rama de Francisco

--- app/Http/Controllers/NuestrasSolicitudesController.php
+++ app/Http/Controllers/NuestrasSolicitudesController.php
@@ -135,7 +135,7 @@ class NuestrasSolicitudesController extends Controller
                 }
                 $esCarta = false;
                 try {
-                    $destinatario->email_envio = "franciscomentadomanzanares@gmail.com";
+                    $destinatario->email_envio = "antonio_sga@yahoo.es";
                     $envio = Mail::send('nuestrasSolicitudes.pdf.cartaSolicitudA1',
                         compact('cursos', 'remitente', 'destinatario', 'fecha_emision', 'esCarta'),
                         function ($message) use ($remitente, $destinatario, $nombreArchivoAdjuntoEmail) {
