7023dcac99cb23440b78513bf76640c070cabbfa
antonio becerra <antonio_sga@yahoo.es>
2016-06-06 18:04:35 +0100

    Añadir filtro esActivo a sus respuestas.

--- resources/views/solicitudesEnviadas/parciales/buscar.blade.php
+++ resources/views/solicitudesEnviadas/parciales/buscar.blade.php
@@ -1,10 +1,14 @@
 {{-- Formulario de busqueda --}}
-{!!FORM::model(Request::only(['comunidades']),['route'=>'solicitudesEnviadas.index','method'=>'GET','role'=>'search']) !!}
+{!!FORM::model(Request::only(['comunidades','esActivo']),['route'=>'solicitudesEnviadas.index','method'=>'GET','role'=>'search']) !!}
 <div class="form-group">
     {!! FORM::select('comunidades', $comunidades, null,array("class"=>"form-control"))!!}
 </div>
 <div class="form-group">
     {!! FORM::select('aceptada', array(''=>'Solicitud Aceptada...','1'=>'Aceptada','0'=>'No Aceptada'),null,array("class"=>"form-control"))!!}
+</div>
+<div class="form-group">
+    {!! FORM::select('esActivo', array(''=>'Todas...','1'=>'Activa','0'=>'No Activa'),
+    null,array("class"=>"form-control"))!!}
 </div>
 <br/>
 <button type="submit" class="btn btn-primary btn-block">
