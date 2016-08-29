4448b0fd756c80a5a4e61f9853f56d8d4565614c
kinderw <franciscomentadomanzanares@gmail.com>
2016-08-29 17:34:25 +0100

    Vista y estilos para el cambio de contraseña, modificación del mensaje enviado al email

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ url('password/reset/'.$token) }}">Haz click aquí para resetear tu password</a>
+<a href="{{ url('auth/password/reset/'.$token) }}">Haz click aqu&iacute; para poder cambiar tu contrase&ntilde;a</a>
