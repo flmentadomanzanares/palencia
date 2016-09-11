8998037fc292eab798cad951f0ce7a14feed1382
kinderw <franciscomentadomanzanares@gmail.com>
2016-09-11 02:26:59 +0100

    Cambio de ruta para la vista de guestGoHome

--- resources/views/comun/guestGoHome.blade.php
+++ resources/views/comun/guestGoHome.blade.php
@@ -3,7 +3,7 @@
     <div width="100%" height="400px" class="row">
         <div class="form-group">
             <div class="heading-caption">Has perdido la sesi&oacute;n</div>
-            <a title="Volver" href="{{route('cursillos.index')}}" class="pull-right">
+            <a title="Volver" href="{{route('/')}}" class="pull-right">
                 <i class="glyphicon glyphicon-arrow-left">
                     <div>Ir login</div>
                 </i>
