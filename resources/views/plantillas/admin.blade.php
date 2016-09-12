7bdc65fccaf0e83520f43432a44157dfcb4fd5b6
kinderw <franciscomentadomanzanares@gmail.com>
2016-09-12 20:54:37 +0100

    Se configura las media para una resolución de 320px en dispositivos móviles

--- resources/views/plantillas/admin.blade.php
+++ resources/views/plantillas/admin.blade.php
@@ -4,7 +4,7 @@
     <title>Palencia</title>
     <meta charset="UTF-8">
     <meta name=description content="">
-    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name=viewport content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
     {!! HTML::style('css/palencia.css') !!}
     @yield("css")
 </head>
