ac44a1130f02979331987367f863d040ede8f68a
fmentado <franciscomentadomanzanares@gmail.com>
2016-11-14 19:10:25 +0000

    Se elimina de direccion.js el obtener localidades

--- public/js/comun/direccion.js
+++ public/js/comun/direccion.js
@@ -9,7 +9,6 @@
             var provincia = $('#select_provincia').val();
             var localidad = $('#select_localidad').val();
             listarProvincias(pais, provincia);
-            listarLocalidades(provincia, localidad);
         };
         var listarProvincias = function (paisId, provincia) {
             $.ajax({
@@ -19,7 +18,7 @@
                 },
                 dataType: "json",
                 type: 'post',
-                url: '/cambiarProvincias',
+                url: '/palencia/public/cambiarProvincias',
                 success: function (data) {
                     var placeHolderProvincia = $('#select_provincia[data-placeholder]');
                     $('#select_provincia').empty();
@@ -33,48 +32,15 @@
                         else
                             $('#select_provincia').append("<option value='" + element.id + "'>" + element.provincia + "</option>");
                     });
-                    if (provincia == undefined) {
-                        listarLocalidades($('#select_provincia option:selected').val());
-                    }
+                }
+            });
+        };
 
-                }
-            });
-        };
-        var listarLocalidades = function (provincia, localidad) {
-            $.ajax({
-                data: {
-                    'provincia_id': provincia,
-                    _token: $('input[name="_token"]').val()
-                },
-                dataType: "json",
-                type: 'post',
-                url: '/cambiarLocalidades',
-                success: function (data) {
-                    $('#select_localidad').empty();
-                    $.each(data, function (key, element) {
-                        if (element.id == localidad)
-                            $('#select_localidad').append("<option selected value='" + element.id + "'>" + element.localidad + "</option>");
-                        else
-                            $('#select_localidad').append("<option value='" + element.id + "'>" + element.localidad + "</option>");
-                    });
-                }
-            });
-        };
         $('#select_pais').change(function (evt) {
             evt.preventDefault();
             if (($('#select_provincia').length == 0 ))
                 return;
             listarProvincias($('#select_pais option:selected').val());
-        });
-        //Cambio Localidades.
-        $('#select_provincia').change(function (evt) {
-            evt.preventDefault();
-            if (($('#select_localidad').length == 0 )) {
-                $("[name='localidad']").val('');
-                return;
-            }
-
-            listarLocalidades($('#select_provincia option:selected').val());
         });
         inicializar();
     });
