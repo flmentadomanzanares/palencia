1c9f11d6545846fa3885587cf75ca2e2edc1cc13
franc <franciscomentadomanzanares@yahoo.es>
2017-07-17 02:16:32 +0100

    Scroll top, scroll bottom

--- public/js/publico/scroll_to_top.js
+++ public/js/publico/scroll_to_top.js
@@ -3,28 +3,43 @@
  */
 $(function () {
 
-    var $BloqueToTop = $(".scroll_to_top");
-    var $BloqueToBottom = $(".scroll_to_top");
+    var $bloqueToTop = $(".scroll_to_top");
+    var $bloqueToBottom = $(".scroll_to_bottom");
     $(document).on("click", ".scroll_to_top", function (evt) {
         evt.preventDefault();
         $("body").animate({
             scrollTop: 0
         }, 600);
     });
+    $(document).on("click", ".scroll_to_bottom", function (evt) {
+        evt.preventDefault();
+        $("body").animate({
+            scrollTop: document.body.scrollHeight
+        }, 600);
+    });
     $(window).scroll(function (evt) {
         evt.preventDefault();
-        mostrarOcultarToTop($(this), $BloqueToTop);
+        mostrarOcultarToTop($(this), $bloqueToTop, $bloqueToBottom);
     });
 
-    mostrarOcultarToTop = function (window, elemToTop) {
-        if (window.length > 0 && elemToTop.length > 0) {
+    mostrarOcultarToTop = function (window, elemToTop, elemToBottom) {
+        if (elemToTop.length > 0) {
             if (window.scrollTop() <= 50) {
                 elemToTop.fadeOut();
             } else {
                 elemToTop.fadeIn();
             }
         }
+        if (elemToBottom.length > 0) {
+            console.log(window.scrollTop() + window.height() + "-" + document.body.offsetHeight + "-" + document.body.scrollHeight);
+            if (window.height() >= document.body.clientHeight
+                || window.scrollTop() + window.height() === document.body.scrollHeight) {
+                elemToBottom.fadeOut();
+            } else {
+                elemToBottom.fadeIn();
+            }
+        }
         return false;
     };
-    mostrarOcultarToTop($(window), $BloqueToTop);
+    mostrarOcultarToTop($(window), $bloqueToTop, $bloqueToBottom);
 });
